38eda8fa4cbf59939615937b292c5ad6755d444f
JennPre <73667735+JennPre@users.noreply.github.com>
2020-11-20 02:34:35 -0500

    Update cachematrix.R
    Resolving Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
+## Assignment: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## Matrix inversion is usually a costly computation and there may be some benefit to
+## caching the inverse of matrix rather than compute it repeatedly.
+
+## Assignment: Write a pair of functions that cache the inverse of a matrix 
+## (makeCacheMatrix, cacheSolve)
+
+## Remember:
+## The operator '<<-' can be used to sign a value to an object in an environment that
+## is different from the current environment.
+
+## Function makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {                            ## For setting the value of the matrix
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x                             ## For getting the the value of the matrix
+        setinverse <- function(inverse) i <<- inverse   ## For setting the value of the inverse
+        getinverse <- function() i                      ## For getting the value of the inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Function cacheSolve
+## Computes the inverse of the special "matriz" returned by makeCaheMatrix above. If 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {                               ## For checking if any value is NULL
+                message("getting cached data")
+                return(i)                               ## Returns the inverse value
+        }
+        data <- x$get()
+        i <- inverse(data, ...)                         ## For calculating the inverse value
+        x$setinverse(i)
+        i
 }
